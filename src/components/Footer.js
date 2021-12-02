958eb50b5bd2150c3814fe61c117bf10d92f2d94
Nicknyr <nick@MacBook-Pro.local>
2021-12-02 16:49:46 -0500

    Removing boilerplate theme pages, installed styled-components for styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,33 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
+
+const StyledFooter = styled.footer`
+  background: black;
+`
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
+      <StyledFooter className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
+          {/* background image */}
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div style={{ maxWidth: '100vw' }} className="columns">
               <div className="column is-4">
                 <section className="menu">
+                <Button>Styled Button</Button>
                   <ul className="menu-list">
                     <li>
                       <Link to="/" className="navbar-item">
@@ -74,6 +79,7 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
+              {/*
                 <a title="facebook" href="https://facebook.com">
                   <img
                     src={facebook}
@@ -103,11 +109,12 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
+                */}
               </div>
             </div>
           </div>
         </div>
-      </footer>
+      </StyledFooter>
     )
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +40,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              {/*<img src={logo} alt="Kaldi" style={{ width: '88px' }} />*/}
             </Link>
             {/* Hamburger menu */}
             <div
@@ -63,9 +61,6 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
@@ -84,7 +79,7 @@ const Navbar = class extends React.Component {
                 rel="noopener noreferrer"
               >
                 <span className="icon">
-                  <img src={github} alt="Github" />
+                  {/*<img src={github} alt="Github" />*/}
                 </span>
               </a>
             </div>
